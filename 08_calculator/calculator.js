6e0d1614540f5f26f9049a2a34ca25e5ba9c0226
Winston Zhang <82705219+winnytp@users.noreply.github.com>
2025-01-03 23:03:24 +1100

    Finished 08-calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  const total = arr.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0); 
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  const total = arr.reduce((accumulator, currentValue) => {
+    return accumulator * currentValue;
+  }, 1);
 
+  return total;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+  let currentTotal = number;
+  for (i = 1; i < power; i++) {
+    currentTotal = currentTotal * number;
+  }
+  return currentTotal;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) return 1;
+
+  let currentMultiplier = number;
+  let result = number;
+
+  for (let i = 1; i < number; i++) {
+    result = result * (currentMultiplier - 1);
+    currentMultiplier--;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
